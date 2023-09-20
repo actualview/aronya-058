8cf65f50618fd57e8217f5fa50cf0052eba52f07
actualview <138713344+actualview@users.noreply.github.com>
2023-09-20 13:46:49 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Tricera',
+            location: {
+                lat: 35.145699,
+                lng: 129.137483,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${35.145699}; longitude: ${129.137483};`);
+        model.setAttribute('gltf-model', './assets/tricera.gltf');
+        model.setAttribute('rotation', '0 15 0');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '1.7 1.7 1.7');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
